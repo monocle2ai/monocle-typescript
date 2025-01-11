f5bf7a986acad8d7647ad5cf7c1462dda5bc8fc3
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-01-12 02:55:11 +0530

    fixes

--- src/exporters/index.js
+++ src/exporters/index.js
@@ -2,7 +2,7 @@ const { FileSpanExporter } = require('./file/FileSpanExporter');
 const { MonocleConsoleSpanExporter } = require('./monocle/MonocleConsoleSpanExporter');
 const monocleExporters = {
     "s3": () => new (require('./aws/AWSS3SpanExporter').AWSS3SpanExporter)({}),
-    "blob": () => new (require('./azure/AzureBlobSpanExporter').AzureBlobSpanExporter)(),
+    "blob": () => new (require('./azure/AzureBlobSpanExporter').AzureBlobSpanExporter)({}),
     "file": () => new FileSpanExporter({}),
     "console": () => new MonocleConsoleSpanExporter(),
 };
