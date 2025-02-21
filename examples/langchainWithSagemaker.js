4f1c896e2aae28dafb9d58c5ea0f6c7f1e8a5bc6
shambhavigoel1002 <shambhavi.g@beehyv.com>
2025-02-21 18:19:18 +0530

    format fix
    Signed-off-by: shambhavigoel1002 <shambhavi.g@beehyv.com>

--- examples/langchainWithSagemaker.js
+++ examples/langchainWithSagemaker.js
@@ -1,10 +1,8 @@
-require("dotenv").config({ path: "../.env" });
-
-const { setupMonocle } = require("monocle2ai");
+const { setupMonocle } = require("../dist");
 
 setupMonocle("langchain.app");
 
-const { OpenAIEmbeddings } = require("@langchain/openai");
+const { ChatOpenAI, OpenAIEmbeddings } = require("@langchain/openai");
 const { formatDocumentsAsString } = require("langchain/util/document");
 const { PromptTemplate } = require("@langchain/core/prompts");
 const { MemoryVectorStore } = require("langchain/vectorstores/memory");
@@ -14,74 +12,35 @@ const {
   RunnablePassthrough
 } = require("@langchain/core/runnables");
 const { StringOutputParser } = require("@langchain/core/output_parsers");
-const AWS = require("aws-sdk");
-
-const getSageMakerResponse = async (inputText) => {
-  let sagemaker = new AWS.SageMakerRuntime({
-    apiVersion: "2017-07-24",
-    region: "us-east-1",
-    credentials: new AWS.Credentials(
-      process.env.AWS_ACCESS_KEY_ID,
-      process.env.AWS_SECRET_ACCESS_KEY,
-      null
-    )
-  });
-  const inputData = {
-    question: "What is coffee?",
-    context: inputText
-  };
-  const params = {
-    EndpointName: "okahu-sagemaker-rag-qa-ep",
-    Body: JSON.stringify(inputData),
-    ContentType: "application/json"
-  };
-
-  try {
-    const data = await sagemaker.invokeEndpoint(params).promise();
-    const responseBody = JSON.parse(Buffer.from(data.Body).toString("utf8"));
-    return responseBody.answer;
-  } catch (err) {
-    console.error("Error invoking SageMaker endpoint:", err);
-    throw err;
-  }
-};
 
 const langchainInvoke = async (msg) => {
+  const model = new ChatOpenAI({});
   const text = "Coffee is a beverage brewed from roasted, ground coffee beans.";
-
   const vectorStore = await MemoryVectorStore.fromTexts(
     [text],
     [{ id: 1 }],
     new OpenAIEmbeddings()
   );
-
   const retriever = vectorStore.asRetriever();
 
   const prompt =
     PromptTemplate.fromTemplate(`Answer the question based only on the following context:
 {context} .
 If you don't know the answer, you can say "I don't know".
-
 Question: {question}`);
 
-  const chain = RunnableSequence.from(
-    [
-      {
-        context: retriever.pipe(formatDocumentsAsString),
-        question: new RunnablePassthrough()
-      },
-      prompt,
-      async ({ context, question }) => {
-        const inputText = `Context: ${context}\nQuestion: ${question}`;
-        const sageMakerResponse = await getSageMakerResponse(inputText);
-        return sageMakerResponse;
-      },
-      new StringOutputParser()
-    ],
-    ["context", "question"]
-  );
+  const chain = RunnableSequence.from([
+    {
+      context: retriever.pipe(formatDocumentsAsString),
+      question: new RunnablePassthrough()
+    },
+    prompt,
+    model,
+    new StringOutputParser()
+  ]);
+
   const res = await chain.invoke(msg);
   return res;
 };
 
-langchainInvoke("What is coffee?").then(console.log).catch(console.error);
+langchainInvoke("What is coffee?").then(console.log);
