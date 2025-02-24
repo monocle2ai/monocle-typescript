5af1c02847be445edf0256a62ac1ddc771d0c869
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-02-25 01:27:28 +0530

    added multi exporter support
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/exporters/index.ts
+++ src/exporters/index.ts
@@ -1,27 +1,43 @@
 import { FileSpanExporter } from './file/FileSpanExporter';
 import { MonocleConsoleSpanExporter } from './monocle/MonocleConsoleSpanExporter';
 import { OkahuSpanExporter } from './okahu/OkahuSpanExporter';
+import { AWSS3SpanExporter } from './aws/AWSS3SpanExporter';
+import { AzureBlobSpanExporter } from './azure/AzureBlobSpanExporter'
+
+
 
 const monocleExporters = {
-    "s3": () => new (require('./aws/AWSS3SpanExporter').AWSS3SpanExporter)({}),
-    "blob": () => new (require('./azure/AzureBlobSpanExporter').AzureBlobSpanExporter)({}),
+    "s3": () => new AWSS3SpanExporter({}),
+    "blob": () => new AzureBlobSpanExporter({}),
     "okahu": () => new OkahuSpanExporter({}),
     "file": () => new FileSpanExporter({}),
     "console": () => new MonocleConsoleSpanExporter(),
 };
 
-function getMonocleExporter() {
-    const exporterName = process.env.MONOCLE_EXPORTER || 'console';
-    let exporter;
+function getMonocleExporters() {
+    const exporterNameList = (process.env.MONOCLE_EXPORTER || 'console').split(',');
 
-    try {
-        exporter = monocleExporters[exporterName]();
-    } catch (ex) {
-        console.warn(`Unsupported Monocle span exporter setting ${exporterName}, using default ConsoleSpanExporter.`);
-        return new MonocleConsoleSpanExporter();
+    let exporters = [];
+    let defaultConsoleExporterAdded = false;
+    for (let exporterName of exporterNameList) {
+        exporterName = exporterName.trim();
+        try {
+            let exporter = monocleExporters[exporterName]();
+            if (exporter) {
+                exporters.push(exporter);
+                continue;
+            }
+        } catch (ex) {
+            console.warn(`Unsupported Monocle span exporter setting ${exporterName}, using default ConsoleSpanExporter.`);
+            if (!defaultConsoleExporterAdded) {
+                exporters.push(new MonocleConsoleSpanExporter());
+                defaultConsoleExporterAdded = true;
+            }
+
+        }
     }
 
-    return exporter;
+    return exporters;
 }
 
-export { getMonocleExporter };
+export { getMonocleExporters };
