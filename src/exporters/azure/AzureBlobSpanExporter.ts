edf2f91e6cd58c98992dff75fdfa9a3325c25995
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-02 17:22:55 +0530

    added fixes for azure and windows
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/exporters/azure/AzureBlobSpanExporter.ts
+++ src/exporters/azure/AzureBlobSpanExporter.ts
@@ -1,6 +1,6 @@
 import { BlobServiceClient } from '@azure/storage-blob';
 import { ExportResultCode } from '@opentelemetry/core';
-import { exportInfo } from '../utils';
+import { exportInfo, getUrlFriendlyTime, makeid } from '../utils';
 import { consoleLog } from '../../common/logging';
 import { Span } from '@opentelemetry/api';
 import { ExportTaskProcessor } from '../taskProcessor/LambdaExportTaskProcessor';
@@ -9,7 +9,6 @@ interface AzureBlobSpanExporterConfig {
     containerName?: string;
     blobPrefix?: string;
     connectionString?: string;
-    fileNameGenerator?: () => string;
     taskProcessor?: ExportTaskProcessor;
 }
 
@@ -17,17 +16,15 @@ class AzureBlobSpanExporter {
     containerName: string;
     blobPrefix: string;
     blobServiceClient: BlobServiceClient;
-    fileNameGenerator: () => string;
     private taskProcessor?: ExportTaskProcessor;
 
-    constructor({ containerName, blobPrefix, connectionString, fileNameGenerator, taskProcessor }: AzureBlobSpanExporterConfig) {
+    constructor({ containerName, blobPrefix, connectionString, taskProcessor }: AzureBlobSpanExporterConfig) {
         this.containerName = containerName || process.env.MONOCLE_BLOB_CONTAINER_NAME || "default-container";
         this.blobPrefix = blobPrefix || process.env.MONOCLE_AZURE_BLOB_PREFIX || "monocle_trace__";
         const blobConnectionString = connectionString || process.env.MONOCLE_BLOB_CONNECTION_STRING || process.env.AZURE_STORAGE_CONNECTION_STRING;
         if (!blobConnectionString)
             throw new Error('Azure Blob Storage connection string is required in  MONOCLE_BLOB_CONNECTION_STRING or AZURE_STORAGE_CONNECTION_STRING');
         this.blobServiceClient = BlobServiceClient.fromConnectionString(blobConnectionString);
-        this.fileNameGenerator = typeof fileNameGenerator == "function" ? fileNameGenerator : () => `${this.blobPrefix}${Date.now().toString()}`;
         this.taskProcessor = taskProcessor;
         if(this.taskProcessor) {
             this.taskProcessor.start();
@@ -60,8 +57,8 @@ class AzureBlobSpanExporter {
     }
 
     async _sendSpans(spans: Span[], done) {
-        const timestamp = Date.now().toString();
-        const blobName = `${this.blobPrefix}/${timestamp}.ndjson`;
+        const timestamp = getUrlFriendlyTime(new Date());
+        const blobName = `${this.blobPrefix}${timestamp}_${makeid(5)}.ndjson`;
         const body = spans.map(span => JSON.stringify(this._exportInfo(span))).join('\n');
         consoleLog('sending spans to Azure Blob Storage:', blobName, body);
 
