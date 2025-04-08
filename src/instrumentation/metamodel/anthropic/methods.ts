c6f6c1bdd0e5c757ef9383eae6ff31fcd66b3af2
shambhavigoel1002 <shambhavi.g@beehyv.com>
2025-04-08 17:13:54 +0530

    updated dependencies and methods
    Signed-off-by: shambhavigoel1002 <shambhavi.g@beehyv.com>

--- src/instrumentation/metamodel/anthropic/methods.ts
+++ src/instrumentation/metamodel/anthropic/methods.ts
@@ -1,25 +1,9 @@
 export const config = [
     {
-        package: "@anthropic-ai/sdk/resources/messages",
-        object: "Messages",
-        method: "create",
-        spanName: "anthropic.messages.create",
-        output_processor: [require("./entities/inference.js").config]
-
-    },
-    {
         package: "@anthropic-ai/sdk/resources/messages/messages",
         object: "Messages",
         method: "create",
         spanName: "anthropic.messages.create",
-        output_processor: [require("./entities/inference.js").config]
-
-    },
-    {
-        package: "@anthropic-ai/sdk/resources/messages",
-        object: "Messages",
-        method: "stream",
-        spanName: "anthropic.messages.stream",
         output_processor: [require("./entities/inference.js").config]
 
     },
