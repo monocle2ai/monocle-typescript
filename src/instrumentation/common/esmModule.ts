0ff9e46f441e5e4d3fd9808725fceb8fcd48cd01
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-04-21 19:05:35 +0530

    added handling for vercel
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/instrumentation/common/esmModule.ts
+++ src/instrumentation/common/esmModule.ts
@@ -1,11 +1,27 @@
 import * as module_private_1 from 'module'
 import { consoleLog } from '../../common/logging'
+import { isVercelEnvironment } from './utils'
 
 // @ts-ignore
 export function registerModule() {
-// @esm-only
-    consoleLog("registering import-in-the-middle/hook.mjs")
-    // @ts-ignore
-    module_private_1.register('import-in-the-middle/hook.mjs', import.meta.url)
-// @end-esm-only
+    // @esm-only
+    try {
+        consoleLog("registering import-in-the-middle/hook.mjs")
+
+        import('import-in-the-middle/hook.mjs')
+
+        // @ts-ignore
+        if (isVercelEnvironment()) {
+            module_private_1.register('import-in-the-middle/hook.mjs', "file:///var/task/node_modules")
+        }
+        else {
+            // @ts-ignore
+            module_private_1.register('import-in-the-middle/hook.mjs', import.meta.url)
+        }
+    }
+    catch (e) {
+        consoleLog("Error registering import-in-the-middle/hook.mjs", e)
+    }
+
+    // @end-esm-only
 }
